0af335abcbe68c0a8b066913f4977bd00a4e420a
FamBoricua2001 <128109420+FamBoricua2001@users.noreply.github.com>
2024-10-11 21:18:28 -0500

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div>
     <h1>{{title}}</h1>
     <h3>{{title1}}</h3>
+    <Discription />
   </div>
 </template>
 
---
+++ src/components/Discription.vue
@@ -0,0 +1,21 @@
+<template>
+    <h4>{{ message }}</h4>
+</template>
+
+<script>
+export default {
+    name: 'Discription',
+    data() {
+        return {
+            message: 'I have always loved websites and how they show the best part of the world.  What is the best way to show the world how great it is than having a website that shows that.  Being from Puerto Rico I love providing unique insight and personality in all my work.  I enjoy old cars, playing basketball and baseball.  I also enjoy collecting sports cards from 1970s - 1990s.'
+        }
+    }
+}
+</script>
+
+<style>
+h4 {
+    color: red;
+    font-family:'Times New Roman', Times, serif;
+ }
+ </style>
