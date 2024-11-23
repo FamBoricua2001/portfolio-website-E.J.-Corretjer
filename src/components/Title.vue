fdd081b734c34aa01ab544511e0e00ff4727a696
FamBoricua2001 <128109420+FamBoricua2001@users.noreply.github.com>
2024-11-22 22:19:57 -0600

    Commented out code that was not need and add a discription of my future goals as a web developer

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
+    <p>My name is E.J. Corretjer I am in the progress of obtaining an associates degree in Infomation Technology with a concentration in Web Debelopment.  I really enjoy working on websites, creating applications and trying to find solutions to problems and prevent problems before they start.  I have always had an interest in all kinds of technology.  I would like to use my new skills in Cyber Security or helping clients create applications or websites that meet their needs. </p>
+    <!--<p> 
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
@@ -26,7 +27,7 @@
       <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    </ul> -->
   </div>
 </template>
 
